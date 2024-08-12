26bb3617addd9722f841ddefa8351d4434d46e4c
pkpotter03 <pkpotter03@gmail.com>
2024-08-12 18:45:06 +0530

    pages commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/my-portfolio' : '',
+  assetPrefix: isProd ? '/my-portfolio/' : '',
+};
 
 module.exports = nextConfig
